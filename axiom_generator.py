c4b75fe79d7eca677a400ec111c0f67f21a80269
PsiWa <serezfed@gmail.com>
2023-09-26 19:14:10 +0300

    Tasks added

--- axiom_generator.py
+++ axiom_generator.py
@@ -1,24 +1,27 @@
+import sys
 import  turtle
 
 class Lsystem:
     def __init__(self,startx,starty,startangle,length,angle,axiom,rules):
+        self.SetParams(startx,starty,startangle,length,angle,axiom,rules)
+    def SetParams(self,startx,starty,startangle,length,angle,axiom,rules):
         self.startx = startx
         self.starty = starty
         self.startangle = startangle
         self.length = length
-        self.angle:float = angle
+        self.angle: float = angle
         self.axiom = axiom
         self.rules = {}
         self.syms = {}
         count = 0;
         for str in rules:
-            str.replace("–", "-")
-            str.replace(" ", "")
-            (sym,rule) = str.split('=')
-            self.syms[count] = sym
-            self.rules[sym] = rule
-            count+=1
-
+            if str != "":
+                str.replace("–", "-")
+                str.replace(" ", "")
+                (sym, rule) = str.split('=')
+                self.syms[count] = sym
+                self.rules[sym] = rule
+                count += 1
 
     def GenerateNSteps(self,n):
         print(self.axiom)
@@ -31,10 +34,11 @@ class Lsystem:
             print(self.axiom)
 
 
-    def Draw(self):
-        wn = turtle.Screen()
-        wn.tracer(0)
-        turtle.speed(10)
+    def Draw(self,isInstant,s):
+        if (isInstant):
+            turtle.Screen().tracer(0)
+            s=int(10)
+        turtle.speed(int(s))
         turtle.penup()
         turtle.goto(self.startx,self.starty)
         turtle.setheading(self.startangle)
@@ -46,7 +50,8 @@ class Lsystem:
                 turtle.left(self.angle)
             if (ch == "-"):
                 turtle.right(self.angle)
-            if (ch.isalpha()):
+            #if (ch.isalpha()):
+            if (ch == "F"):
                 turtle.forward(self.length)
             if (ch == "["):
                 returnpoints.append(turtle.pos())
@@ -56,8 +61,7 @@ class Lsystem:
                 turtle.goto(returnpoints.pop())
                 turtle.setheading(returnangle.pop())
                 turtle.pendown()
-        wn.update()
+        turtle.Screen().update()
         turtle.screensize(5000,5000)
         turtle.exitonclick()
-
 
--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-import turtle
 from axiom_generator import Lsystem
+from os import listdir
+from os.path import isfile, join
+
 
 def main():
-    axiom = "A"
-    rules = ["A=A-B--B+A++AA+B-","B=+A-BB--B-A++A+B"]
-    lsys = Lsystem(0,0,0,10,60,axiom,rules)
-    lsys.GenerateNSteps(6)
-    lsys.Draw()
+    #axiom = "A"
+    #rules = ["A=A-B--B+A++AA+B-","B=+A-BB--B-A++A+B","",""]
+    rules = []
+    setups = {}
+    onlyfiles = [f for f in listdir("./Tasks") if isfile(join("./Tasks", f))]
+    print(onlyfiles)
+    n = input("select X.txt :")
+    file = open(f"Tasks/{n}.txt", "r")
+    pars=file.read()
+    lines = pars.splitlines()
+    for line in lines:
+        (par,var) = line.split(":")
+        if par == "rule":
+            rules.append(var)
+        else:
+            setups[par]=var
+    lsys = Lsystem(int(setups["startx"]),int(setups["starty"]),float(setups["startangle"]),int(setups["length"]),float(setups["angle"]),setups["axiom"],rules)
+    lsys.GenerateNSteps(int(setups["steps"]))
+    lsys.Draw(True,10)
 
 if __name__ == "__main__":
     main()
